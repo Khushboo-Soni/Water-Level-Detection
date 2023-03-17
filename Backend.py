6ed4c96735f34cebaac95ad9d4f0cfbe8dbf3e19
Khushboo-Soni <126482630+Khushboo-Soni@users.noreply.github.com>
2023-03-17 14:28:16 +0530

    Add files via upload

---
+++ Backend.py
@@ -0,0 +1,81 @@
+def feedback():
+    import streamlit as st
+    st.markdown('* We will provide you water in next 6 hours.')
+    st.markdown('* Please use the water according to use. Don\'t waste water')
+    st.markdown('* If the water is not available we well contact you as soon as possible and guide you '
+                'for that through the phone call provided by you')
+    st.markdown('* For Contact:-\n'
+                '1. Email: khushboo.21jice014@jietjodhpur.ac.in\n'
+                '2. Contact number: 9983177800')
+
+
+def feedback_tourist():
+    import streamlit as st
+    feedback()
+    st.markdown('**Note**:- The water used by tourist is charged according to the usage amount explained on app info.')
+
+
+def domestic_entries():
+    import streamlit as st
+    name = st.text_input('Enter Name *')
+    address = st.text_input('Enter Address *')
+    pincode = st.text_input('Enter Pincode *')
+    aadhar = st.text_input('Enter Aadhar card number *')
+    contact = st.text_input('Enter Contact number *')
+    count = st.text_input('Enter Count of family members *')
+    response = st.radio('Do you have animals?', ('Yes', 'No'), index=1)
+    c1 = c2 = c3 = c4 = c5 = 0
+    if response == 'Yes':
+        st.write('Choose animals you have *')
+        op1 = st.checkbox('Cow')
+        op2 = st.checkbox('Buffalo')
+        op3 = st.checkbox('Sheep')
+        op4 = st.checkbox('Dog')
+        op5 = st.checkbox('Hen')
+
+        st.write('Number of each animals *')
+        c1 = st.text_input('Enter number of cows *', value='0')
+        c2 = st.text_input('Enter number of buffaloes *', value='0')
+        c3 = st.text_input('Enter number of sheep *', value='0')
+        c4 = st.text_input('Enter number of dogs *', value='0')
+        c5 = st.text_input('Enter number of hens *', value='0')
+
+    return name, address, pincode, aadhar, contact, count, c1, c2, c3, c4, c5
+
+
+def tourist_entries():
+    import streamlit as st
+    form, info = st.tabs(['Details', 'Info'])
+    with form:
+        name = st.text_input('Enter Hotel name *')
+        address = st.text_input('Enter Address *')
+        pincode = st.text_input('Enter Pincode *')
+        uid = st.text_input('Enter Unique ID of hotel *')
+        contact = st.text_input('Enter Contact number *')
+        count = st.text_input('Enter Count of guest *')
+        citizen = st.text_input('Enter Nationality *')
+        checkin = st.time_input('Enter Check-in time *')
+        checkout = st.time_input('Enter Check-out time *')
+    with info:
+        st.write('1. As per the guidelines, the water being used by the tourists per day is '
+                 'at the rate of head charge of 8% of the total charge per day.')
+        st.write('2. For Indian tourists the charges would be 8% of the total per head per day.')
+        st.write('3. For VIP tourists the charges would be 15% of the total per head per day.')
+        st.write('4. If there is lack of water in the main overhead tank of the city/district, '
+                 'the charges may rise with 2% with previous. [For VIPs\' the addition will be of 4%.]')
+        st.write('5. If water would be used mindfully by the tourist, while checking out 2% of the '
+                 'total surplus charges would be returning to the payer/tourists [if not then at the end of '
+                 'the month the amount will be transferred to the payer.]')
+    return name, address, pincode, uid, contact, count, citizen, checkin, checkout
+
+
+def agriculture_entries():
+    import streamlit as st
+    name = st.text_input('Enter Name *')
+    address = st.text_input('Enter Address *')
+    pincode = st.text_input('Enter Pincode *')
+    contact = st.text_input('Enter Contact number *')
+    area = st.text_input('Enter land area (sq. ft.)*')
+    water_needed = st.text_input('Enter Water needed *')
+    crope_type = st.text_input('Enter Crope type *')
+    return name, address, pincode, contact, area, water_needed, crope_type
---
+++ Home.py
@@ -0,0 +1,118 @@
+import streamlit as st
+from Backend import *
+from csv import DictWriter
+import os
+
+# adding page name
+st.set_page_config(page_title='Water Level', page_icon='💧')
+
+# adding logo image
+st.image('logo.jpeg')
+
+# adding title
+st.header('We Love Water')
+
+# adding radio button
+use_type = st.radio('Select your usage type', ('Domestic', 'Tourist', 'Agriculture'))
+
+# creating container
+contain = st.empty()
+
+# declaring fields
+name = address = pincode = aadhar = contact = count = uid = citizen = checkin = checkout = \
+    area = water_needed = crope_type = None
+c1 = c2 = c3 = c4 = c5 = 0
+
+# adding choose condition
+if use_type == 'Domestic':
+    with contain.container():
+        name, address, pincode, aadhar, contact, count, c1, c2, c3, c4, c5 = domestic_entries()
+
+elif use_type == 'Tourist':
+    with contain.container():
+        name, address, pincode, uid, contact, count, citizen, checkin, checkout = tourist_entries()
+else:
+    with contain.container():
+        name, address, pincode, contact, area, water_needed, crope_type = agriculture_entries()
+
+back = st.empty()
+submit = st.empty()
+
+# adding submit button
+with submit.container():
+    submit_button = st.button('Submit')
+
+# adding back button
+with back.container():
+    if st.button('Back'):
+        contain.empty()
+        back.empty()
+        submit.empty()
+
+# checking for submit condition
+if submit_button:
+    # save data for domestic
+    if use_type == 'Domestic':
+        with open('domestic_record.csv', 'a') as f:
+            dict_writer = DictWriter(f, fieldnames=['Name', 'Address', 'Pincode', 'Aadhar No.', 'Contact No',
+                                                    'Family size', 'Animal name', 'Animal Count', 'Water Required'])
+            if os.stat('domestic_record.csv').st_size == 0:
+                dict_writer.writeheader()
+            dict_writer.writerow({
+                'Name': name,
+                'Address': address,
+                'Pincode': pincode,
+                'Aadhar No.': aadhar,
+                'Contact No': contact,
+                'Family size': count,
+                'Animal name': ['Cow', 'Buffalo', 'Dog', 'Sheep', 'Hen'],
+                'Animal Count': [c1, c2, c3, c4, c5],
+                'Water Required': 20 * int(count) + 80 * int(c1) + 100 * int(c2)
+                                  + 10 * int(c3) + 2 * int(c4) + 5 * int(c5)
+            })
+
+    # save data for tourist
+    elif use_type == 'Tourist':
+        with open('tourist_record.csv', 'a') as f:
+            dict_writer = DictWriter(f, fieldnames=['Name', 'Address', 'Pincode', 'Unique ID', 'Contact No',
+                                                    'No of Guests',
+                                                    'Nationality', 'Check-in', 'Check-out', 'Water Required'])
+            if os.stat('tourist_record.csv').st_size == 0:
+                dict_writer.writeheader()
+            dict_writer.writerow({
+                'Name': name,
+                'Address': address,
+                'Pincode': pincode,
+                'Unique ID': uid,
+                'Contact No': contact,
+                'No of Guests': count,
+                'Nationality': citizen,
+                'Check-in': checkin,
+                'Check-out': checkout,
+                'Water Required': 18 * int(count)
+            })
+
+    # save data for agriculture
+    else:
+        with open('agriculture_record.csv', 'a') as f:
+            dict_writer = DictWriter(f, fieldnames=['Name', 'Address', 'Pincode', 'Contact No', 'Land Area',
+                                                    'Crope Type', 'Water Required'])
+            if os.stat('agriculture_record.csv').st_size == 0:
+                dict_writer.writeheader()
+            dict_writer.writerow({
+                'Name': name,
+                'Address': address,
+                'Pincode': pincode,
+                'Contact No': contact,
+                'Land Area': area,
+                'Crope Type': crope_type,
+                'Water Required': water_needed
+            })
+
+    contain.empty()
+    if use_type == 'Tourist':
+        feedback_tourist()
+    else:
+        feedback()
+    back.empty()
+    submit.empty()
